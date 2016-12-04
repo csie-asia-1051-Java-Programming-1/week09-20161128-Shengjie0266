ef1dd187cb355b41f5aceeb143706d83cfcc888b
Shengjie0266 <105021040@live.asia.edu.tw>
2016-12-04 23:11:52 +0800

    week09

--- src/hw/hw01.java
+++ src/hw/hw01.java
@@ -1,15 +1,33 @@
 package hw;
-/*
- * Topic: 讓使用者輸入一正整數，利用函式呼叫找出一連續整數數列讓該數列的和為此一正整數，有些正整數並不能找出連續整數，若找不到時，請印出「No」。
- 例如: 15 =1+2+3+4+5 =4+5+6=7+8
- * Date: 2016/11/28
- * Author: 1050210XX 周永振老師
- */
+import java.util.Scanner;
 public class hw01 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		Scanner scn=new Scanner(System.in);
+		int n=scn.nextInt();
+		fun1(n);
 	}
-
+	public static void fun1(int x){
+		String str ="";
+		int sum=0,count=0;
+		for(int i=1;i<x;i++){
+			sum=0;
+			str=""+i;
+			int j=i;
+			while(sum<x){
+				sum+=j++;
+				if(sum<x){
+					str+="+"+j;
+				}
+			}
+			if(sum==x){
+				count ++;
+				System.out.println(str);
+			}
+		}
+		if(count==0){
+			System.out.println("No");
+		}
+	}
 }
